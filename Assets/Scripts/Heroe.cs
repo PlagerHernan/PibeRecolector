b066bdd30cc427009a2b3fd373382ff25e97a6ab
PlagerHernan <plagerhernan@gmail.com>
2020-08-08 13:59:40 -0300

    Agregado movimiento Heroe

---
+++ Assets/Scripts/Heroe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroe : MonoBehaviour
+{
+    [SerializeField] float _velocity;
+    float _xMovement;
+
+    void Update()
+    {
+        _xMovement = Input.GetAxisRaw("Horizontal") * _velocity;
+        transform.position += new Vector3(_xMovement, 0f, 0f) * Time.deltaTime;
+    }
+}
