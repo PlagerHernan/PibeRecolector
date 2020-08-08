2df8993abaa408c3bc9b6088fbb6841666cd7785
PlagerHernan <plagerhernan@gmail.com>
2020-08-08 12:41:45 -0300

    Agregado Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _timeLevel;
+    float _decimalTimeLevel; 
+
+    private void Start() 
     {
-        
+        _decimalTimeLevel = _timeLevel; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_timeLevel > 0)
+        {   
+            _decimalTimeLevel -= Time.deltaTime; 
+            _timeLevel = Mathf.RoundToInt(_decimalTimeLevel);
+            GetComponent<Text>().text = "TIME \n" + _timeLevel.ToString();
+        }
     }
 }
