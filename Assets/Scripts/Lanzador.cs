5055b20896bee034bf26e26a819ea332e4ce3ed0
PlagerHernan <plagerhernan@gmail.com>
2020-08-10 01:09:18 -0300

    Agregado animaciones de Lanzador

---
+++ Assets/Scripts/Lanzador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lanzador : MonoBehaviour
+{
+    [SerializeField] GameObject _rocaPrefab;
+    [SerializeField] float _timeBetweenThrows; 
+    Animator _animator; 
+
+    void Awake() 
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Start() 
+    {
+        InvokeRepeating("Throw", _timeBetweenThrows, _timeBetweenThrows);
+    }
+
+    void Throw()
+    {
+        _animator.SetTrigger("throw");
+
+        Instantiate(_rocaPrefab, transform.position, new Quaternion());
+    }
+}
